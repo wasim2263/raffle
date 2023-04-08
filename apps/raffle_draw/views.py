d217aba22a57317ec2015d9f13c0cddf261f5c24
wasim2263 <abdullahwasim42@gmail.com>
2023-04-08 04:43:36 +0300

    modified view

--- apps/raffle_draw/views.py
+++ apps/raffle_draw/views.py
@@ -1,5 +1,5 @@
 # views.py
-from django.db import transaction
+from django.db import transaction, IntegrityError
 from django.utils.decorators import method_decorator
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -17,23 +17,25 @@ class PrizeViewSet(viewsets.ModelViewSet):
 class RaffleViewSet(viewsets.ModelViewSet):
     queryset = Raffle.objects.order_by('-created').all()
     serializer_class = RaffleSerializer
-
-    @method_decorator(manager_ips_only)
     @transaction.atomic
+    @method_decorator(manager_ips_only)
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        prizes_data = serializer.validated_data.pop('prizes')
         # Get data from request
         name = serializer.validated_data['name']
         total_tickets = serializer.validated_data['total_tickets']
-
+        prizes_data = serializer.validated_data['prizes']
         # Create Raffle object
         raffle = Raffle(name=name, total_tickets=total_tickets, available_tickets=total_tickets)
         raffle.save()
-        # Create Prize objects and associate with Raffle
-        for prize_data in prizes_data:
-            prize = Prize(name=prize_data['name'], amount=prize_data['amount'], raffle=raffle)
-            prize.save()
-        serializer = self.get_serializer(raffle)
+        try:
+            with transaction.atomic():
+                # Create Prize objects and associate with Raffle
+                for prize_data in prizes_data:
+                    prize = Prize(name=prize_data['name'], amount=prize_data['amount'], raffle=raffle)
+                    prize.save()
+                serializer = self.get_serializer(raffle)
+        except Exception as exception:
+            raffle.delete()
         return Response(serializer.data, status=201)
